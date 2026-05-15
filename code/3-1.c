06738d894486b399ac3ab69832a3b1e53c2a89c9
ldh0906 <147708897+ldh0906@users.noreply.github.com>
2026-05-15 09:55:29 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -8,7 +8,7 @@ int mul(int a, int b) { return a * b; }
 
 int compute(int a, int b, int(*fp)(int, int)) {
     if(fp == NULL) {
-        printf("NULL pointer\n");
+        printf("Null pointer\n");
         return -1;
     } else {
         return fp(a, b);
